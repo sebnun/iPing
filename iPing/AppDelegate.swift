1d84e007ded1d1d58e0e8eda05cb28cdd13cc905
Sebastian <sebnun@gmail.com>
2016-05-18 19:10:22 -0300

    ayyy

---
+++ iPing/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iPing
+//
+//  Created by Sebastian on 5/18/16.
+//  Copyright © 2016 Sebastian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iPing/ViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  iPing
+//
+//  Created by Sebastian on 5/18/16.
+//  Copyright © 2016 Sebastian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    
+    @IBOutlet var outputView: NSTextView!
+    @IBOutlet weak var hostField: NSTextField!
+    @IBOutlet weak var startButton: NSButton!
+    var task: NSTask?
+    var pipe: NSPipe?
+    var fileHandle: NSFileHandle?
+    
+    @IBAction func togglePinging(sender: NSButton) {
+        // Is there a running task?
+        if let task = task {
+            // If there is, stop it!
+            task.interrupt()
+        } else {
+            // If there isn't, start one.
+            
+            // Create a new task
+            let task = NSTask()
+            task.launchPath = "/sbin/ping"
+            task.arguments = ["-c10", hostField.stringValue]
+            
+            // Create a new pipe for standardOutput
+            let pipe = NSPipe()
+            task.standardOutput = pipe
+            
+            // Grab the file handle
+            let fileHandle = pipe.fileHandleForReading
+            
+            self.task = task
+            self.pipe = pipe
+            self.fileHandle = fileHandle
+            
+            let notificationCenter = NSNotificationCenter.defaultCenter()
+            notificationCenter.removeObserver(self)
+            notificationCenter.addObserver(self,
+                                           selector: #selector(ViewController.receiveDataReadyNotification(_:)),
+                                           name: NSFileHandleReadCompletionNotification,
+                                           object: fileHandle)
+            notificationCenter.addObserver(self,
+                                           selector: #selector(ViewController.receiveTaskTerminatedNotification(_:)),
+                                           name: NSTaskDidTerminateNotification,
+                                           object: task)
+            
+            task.launch()
+            
+            outputView.string = ""
+            
+            fileHandle.readInBackgroundAndNotify()
+        }
+        
+    }
+    
+    
+    func appendData(data: NSData) {
+        let string = String(data: data, encoding: NSUTF8StringEncoding)! as String
+        let textStorage = outputView.textStorage!
+        let endRange = NSRange(location: textStorage.length, length: 0)
+        textStorage.replaceCharactersInRange(endRange, withString: string)
+    }
+    
+    
+    func receiveDataReadyNotification(notification: NSNotification) {
+        let data = notification.userInfo![NSFileHandleNotificationDataItem] as! NSData
+        let length = data.length
+        
+        print("received data: \(length) bytes")
+        if length > 0 {
+            self.appendData(data)
+        }
+        
+        // If the task is running, start reading again
+        if let fileHandle = fileHandle {
+            fileHandle.readInBackgroundAndNotify()
+        }
+    }
+    
+    
+    func receiveTaskTerminatedNotification(notification: NSNotification) {
+        print("task terminated")
+        
+        task = nil
+        pipe = nil
+        fileHandle = nil
+        
+        startButton.state = 0
+    }
+    
+}
+
